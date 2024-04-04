6d76c6eabd245380e30cc02404aa2cac070f2b09
hbahramian <habahram@iu.edu>
2024-04-04 10:13:12 -0400

    .

---
+++ register.js
@@ -0,0 +1,30 @@
+
+
+async function signup() {
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let customer = {email:email, username: username, password: password}
+    let request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(customer)
+      };
+      try {
+        let response = await fetch(getHost() + "/signup", request);
+        if(response.status == 200) {  
+            alert("The registration was successful!")
+            location.href = "login.html";
+
+        } else {
+            console.log(`response status:${response.status}`);            
+            alert("Something went wrong!");
+        }
+      }
+      catch(error) {
+        console.log(error);        
+        alert("Something went wrong!");
+      }    
+}
