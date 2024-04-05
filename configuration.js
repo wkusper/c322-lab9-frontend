41fac93179156b732bcc4f086accc796e2e24a88
Will Kusper <wkusper@iu.edu>
2024-04-05 12:50:50 -0400

    added render link

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 0;
 
 const host_local = "http://localhost:8080";
-const host_remote = "https://ducks-service-???.onrender.com";
+const host_remote = "https://ducks-service-latest-1-mann.onrender.com";
 
 function getHost() {
   return mode == 0 ? host_local : host_remote;
